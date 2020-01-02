670a3f8e7633581b3dce3b345ff3535a9002d7f3
James Canady <jecanady@us.ibm.com>
2020-01-01 18:39:29 -0600

    Added additional unit tests for UI

--- src/MarsImages/ClientApp/src/App.test.js
+++ src/MarsImages/ClientApp/src/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {render, fireEvent, screen} from '@testing-library/react'
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './components/Images/Header';
 import ImageResult from './components/Images/ImageResult';
 import ImageResults from './components/Images/ImageResults';
+import ImageDateSelection from './components/Images/ImageDateSelection';
 import ImageSearch from './components/Images/ImageSearch';
 import NoResultsFound from './components/Images/NoResultsFound';
 
@@ -23,6 +25,7 @@ it('renders without crashing', async () => {
     await new Promise(resolve => setTimeout(resolve, 1000));
 });
 
+//  ImageResults tests
 it('ImageResults Should return null I', async () => {
     const component = ReactDOM.render(<ImageResults />, container);
     expect(component).toBe(null);
@@ -40,6 +43,7 @@ it('ImageResults Should return null II', async () => {
     expect(component).toBe(null);
 });
 
+//  ImageResult tests
 it('ImageResult should return null', async () => {
     const component = ReactDOM.render(<ImageResult />, container);
     expect(component).toBe(null);
@@ -58,4 +62,54 @@ it('ImageResult should Contain formated Name', async () => {
     };
     const component = ReactDOM.render(<ImageResult metaData={metaData} />, container);
     expect(container.textContent).toContain('ABC - 123');
+});
+
+//  Header Tests
+it('Header Should contain Brand', async () => {
+    const component = ReactDOM.render(<Header />, container);
+    expect(container.textContent).toContain('Mars Images');
+});
+
+//  ImageDateSelection
+it('ImageDateSelection should render I', async () => {
+    //  Stateless components are not returned by render
+    const component = ReactDOM.render(<ImageDateSelection />, container);
+    expect(component).toBe(null);
+});
+
+it('ImageDateSelection should render II', async () => {
+    //  Stateless components are not returned by render
+    ReactDOM.render(<ImageDateSelection />, container);
+    expect(container.getElementsByTagName('option').length).toBe(1);
+});
+
+it('ImageDateSelection should render III', async () => {
+    //  Stateless components are not returned by render
+    var results = [
+        {
+            date:'2017-02-222',
+            name: 'Bob',
+            status: 'Pending'
+        }
+    ];
+    ReactDOM.render(<ImageDateSelection results={results} />, container);
+    expect(container.getElementsByTagName('option').length).toBe(2);
+});
+
+it('ImageDateSelection should render III', async () => {
+    //  Stateless components are not returned by render
+    var results = [
+        {
+            date:'2017-02-21',
+            name: 'Bob I',
+            status: 'Pending'
+        },
+        {
+            date:'2017-02-22',
+            name: 'Bob II',
+            status: 'Pending'
+        }
+    ];
+    ReactDOM.render(<ImageDateSelection results={results} />, container);
+    expect(container.getElementsByTagName('option').length).toBe(3);
 });
